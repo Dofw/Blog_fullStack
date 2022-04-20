0ed803542a2f27487e818792630721db0c9b5b62
Dofw <l>
2022-04-20 22:29:18 +0800

    使用vue官网颜色

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,81 +1,40 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+const onClick = () => {
+  const bodyDom = document.body
+  const names = bodyDom.getAttribute('class')
+  console.log(names)
+
+  if (names && names.indexOf('dark') > -1) {
+    bodyDom.classList.remove('dark')
+
+  } else {
+
+    bodyDom.classList.add('dark')
+  }
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="wrapper" @click="onClick">
+    <div class="child"></div>
+  </div>
 </template>
 
 <style>
-@import './assets/base.css';
+@import './assets/scss/base.scss';
+@import './assets/scss/theme.scss';
 
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
+.wrapper {
+  width: 100px;
+  height: 100px;
+  border: 1px solid green;
+  background-color: var(--color-background);
 
-  font-weight: normal;
 }
 
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.child {
+  background-color: var(--color-background);
 }
 </style>
