998905189b9ce403ef66522d4988d7cd7b78a9f6
Dofw <l>
2022-05-31 19:22:34 +0800

    merge

--- admin/src/components/LayoutHome/index.vue
+++ admin/src/components/LayoutHome/index.vue
@@ -1,19 +1,17 @@
 <template>
-  <div class="homeLayoutContainer">
+  <el-container>
+    <el-aside>
+      <slot name="aside" width="200px"></slot>
+    </el-aside>
     <el-container>
-      <el-aside>
-        <slot name="aside" width="200px"></slot>
-      </el-aside>
-      <el-container>
-        <el-header>
-          <slot name="header"></slot>
-        </el-header>
-        <el-main>
-          <slot name="main"></slot>
-        </el-main>
-      </el-container>
+      <el-header>
+        <slot name="header"></slot>
+      </el-header>
+      <el-main>
+        <slot name="main"></slot>
+      </el-main>
     </el-container>
-  </div>
+  </el-container>
 </template>
 
 <script setup lang="ts">
@@ -21,9 +19,4 @@
 </script>
 
 <style scoped lang="scss">
-// 位于app中
-.homeLayoutContainer {
-  width: 100%;
-  height: 100%;
-}
 </style>
