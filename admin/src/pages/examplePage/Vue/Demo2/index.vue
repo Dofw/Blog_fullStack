29281e486aa9b9903c8da3c0beaa1a38c51a2db8
Dofw <l>
2022-12-18 22:56:04 +0800

    1218demo2展示风格基本搞定

--- admin/src/pages/examplePage/Vue/Demo2/index.vue
+++ admin/src/pages/examplePage/Vue/Demo2/index.vue
@@ -1,20 +1,30 @@
 <template>
   <div class="demo1-container">
-    <!-- 描述 -->
-    <div class="description">
-      <el-descriptions title="vue-compiler-xxx" size="small">
-        <el-descriptions-item class-name="my-content" label="简介:">1.基于compiler-sfc，实现Vue模板编译例子。2. 了解monaco-editor的基本使用。</el-descriptions-item>
-      </el-descriptions>
-    </div>
-    <!-- 编辑-预览-示例 -->
-    <div style="margin-top: 5px; width: 100%; height: calc(100% - 60px)">
-      <MonacoEditor :tempCode="tempCode"></MonacoEditor>
+    <div class="description">简介: vue-compiler-sfc 实现编译效果。全局引入ElementPlus、Vue3的CDN。</div>
+    <div class="preview-wrapper">
+      <!-- 组件集合 -->
+      <el-tabs class="view" type="border-card">
+        <el-tab-pane lazy label="index.vue">
+          <MonacoEditor :tempCode="tempCode"></MonacoEditor>
+        </el-tab-pane>
+        <el-tab-pane lazy label="index2.vue">
+          <MonacoEditor :tempCode="tempCode"></MonacoEditor>
+        </el-tab-pane>
+      </el-tabs>
+
+      <!-- 效果及总结 -->
+      <el-tabs class="view" type="border-card">
+        <el-tab-pane label="demo">
+          <div class="view-dome__wrapper"></div>
+        </el-tab-pane>
+        <el-tab-pane label="summary"> 总结： 1.基于compiler-sfc，实现Vue模板编译例子。2. 了解monaco-editor的基本使用。 </el-tab-pane>
+      </el-tabs>
     </div>
   </div>
 </template>
 <script lang="ts">
 export default {
-  name: "vue-demo-1"
+  name: "vue-demo-2"
 }
 </script>
 <script setup lang="ts">
@@ -23,17 +33,84 @@ import { tempCode } from "./code"
 </script>
 
 <style scoped lang="scss">
-.my-content {
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-}
 .demo1-container {
   width: 100%;
   height: 99%;
-  overflow: hidden;
+  padding: 10px;
+  background: transparent;
+
+  $titleHeight: 60px;
+  $radius: 10px;
   .description {
-    height: 60px;
+    height: $titleHeight;
+    display: flex;
+    align-items: center;
+
+    // border
+    position: relative;
+
+    &::before {
+      content: "";
+      display: block;
+      width: 5px;
+      height: 70%;
+      position: absolute;
+      top: 50%;
+      left: -15px;
+      transform: translateY(-50%);
+      border-left: 10px solid;
+      border-radius: 2px;
+      @include theme-borderColor(vt-c-borderColor-light);
+    }
+  }
+
+  .preview-wrapper {
+    display: flex;
+    height: calc(100% - #{$titleHeight});
+    background: transparent;
+
+    > div {
+      border-radius: $radius;
+      height: 100%;
+      width: 50% !important;
+
+      &.view {
+        .view-dome__wrapper {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+
+          height: 100%;
+          // 新拟态
+          border-radius: 10px;
+          background: linear-gradient(145deg, #cacaca, #f0f0f0);
+          box-shadow: 5px 5px 40px #9d9d9d, 0px 0px 40px #ffffff;
+        }
+
+        :deep() {
+          $tabsHeaderH: 40px;
+          .el-tabs__content {
+            height: calc(100% - #{$tabsHeaderH});
+            background: transparent;
+
+            .el-tab-pane {
+              height: 100%;
+              border-radius: $radius;
+            }
+          }
+
+          .el-tabs__header {
+            height: $tabsHeaderH;
+            border-top-left-radius: $radius;
+            border-top-right-radius: $radius;
+            .el-tabs__item {
+              border-top-left-radius: $radius;
+              border-top-right-radius: $radius;
+            }
+          }
+        }
+      }
+    }
   }
 }
 </style>
