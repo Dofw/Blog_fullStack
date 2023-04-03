9083ee513b31be7d2961fc37e3d6b6fe3833123c
luyujie <1686661978@qq.com>
2023-04-03 18:19:28 +0800

    0403 compiler demo注释掉，cdn访问出现问题

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -3,16 +3,16 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { onMounted } from "vue"
 //初始设置[data-theme_light]
 onMounted(() => {
   const htmlDom = document.documentElement
-  htmlDom.dataset.theme_light = ''
+  htmlDom.dataset.theme_light = ""
 })
 </script>
 
 <style scoped lang="scss">
-:global(#app){
+:global(#app) {
   width: 100%;
   height: 100%;
 }
