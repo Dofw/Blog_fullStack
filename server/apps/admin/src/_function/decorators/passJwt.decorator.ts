b9bb463f03bc4b673dc2fa4d70deae317604557c
luyujie <1686661978@qq.com>
2023-09-01 15:20:57 +0800

    0901迁出server模块

--- server/apps/admin/src/_function/decorators/passJwt.decorator.ts
+++
@@ -1,7 +0,0 @@
-// SetMetadata 工厂函数
-import { SetMetadata } from '@nestjs/common';
-
-export const PASS_JWT = 'passJwt';
-export function PassJwt() {
-  return SetMetadata(PASS_JWT, true);
-}
--- server/apps/admin/src/auth/dto/loginUser.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export default class LoginDto {
-  @ApiProperty({ description: '用户名', example: 'admin' })
-  username: string;
-
-  @ApiProperty({ description: '用户密码', example: '12345678' })
-  password: string;
-}
